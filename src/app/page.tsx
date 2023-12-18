eb04191bfd2e5077d14c5f02c1352feeb5e0b505
Coffee58511867 <marobahlalele910@gmail.com>
2023-12-18 15:04:38 +0200

    Modified readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,24 +6,41 @@ const fetcher = async (url: string) => {
   const response = await fetch(url);
   return response.json();
 };
-
+interface IUser {
+  name: string;
+  phone: string;
+}
 export default function Home() {
   const { data, error } = useSWR(
     "https://jsonplaceholder.typicode.com/users",
     fetcher
   );
 
-  console.log(data);
-
   // Handle loading state
-  if (!data && !error) return <div>Loading...</div>;
+  if (!data && !error) return <div>
+    <div className="flex h-screen items-center justify-center text-2xl text-blue-700">
+      <p>Loading...</p>
+    </div>
+    </div>;
 
   // Handle error state
   if (error) return <div>Failed to load</div>;
 
   return (
     <div>
-      <h3>Home Page</h3>
+      <div className="p-10">
+        <h3 className="text-3xl p-2 text-blue-500">Home Page</h3>
+        <div className="p-2 mt-4 w-full bg-gray-200 rounded-md min-h-48">
+          <h3 className="font-bold text-lg">Users list</h3>
+          <div className="mt-4">
+            {data.map((user: IUser, index: number) => (
+              <div key={index}>
+                <p>{user.name}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
