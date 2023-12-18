610bd0997ba63fb8a03282ae64363fbf7794d614
Coffee58511867 <marobahlalele910@gmail.com>
2023-12-18 14:47:38 +0200

    Modified readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,26 @@
-import Image from "next/image";
+"use client";
+import useSWR from "swr";
+
+// Define the fetcher function
+const fetcher = async (url: string) => {
+  const response = await fetch(url);
+  return response.json();
+};
 
 export default function Home() {
+  const { data, error } = useSWR(
+    "https://jsonplaceholder.typicode.com/users",
+    fetcher
+  );
+
+  console.log(data);
+
+  // Handle loading state
+  if (!data && !error) return <div>Loading...</div>;
+
+  // Handle error state
+  if (error) return <div>Failed to load</div>;
+
   return (
     <div>
       <h3>Home Page</h3>
