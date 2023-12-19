49c388de91cb9552058189de3bfdcb5647d2eb69
Coffee58511867 <marobahlalele910@gmail.com>
2023-12-19 09:42:05 +0200

    Added chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 import useSWR from "swr";
-import ApexCharts from "apexcharts";
-import dynamic from "next/dynamic";
-const ApexChart = dynamic(() => import("react-apexcharts"), { ssr: false });
+import { BarChart, Bar } from "recharts";
 
 // Define the fetcher function
 const fetcher = async (url: string) => {
@@ -32,24 +30,50 @@ export default function Home() {
   // Handle error state
   if (error) return <div>Failed to load</div>;
 
-  if (data) {
-    // Extract user names and phone numbers for chart
-    const chartData = data.map((user: IUser) => ({
-      x: user.name,
-      y: parseInt(user.phone.replace(/\D/g, ""), 10),
-    }));
-
-    // Set up chart options
-    const chartOptions = {
-      chart: {
-        type: "bar",
-        height: 350,
-      },
-      xaxis: {
-        categories: chartData.map((item: any) => item.x),
-      },
-    };
-  }
+  const data2 = [
+    {
+      name: "Page A",
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: "Page B",
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: "Page C",
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: "Page D",
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: "Page E",
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: "Page F",
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: "Page G",
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
 
   return (
     <div>
@@ -65,6 +89,11 @@ export default function Home() {
             ))}
           </div>
         </div>
+      </div>
+      <div className="p-2">
+        <BarChart width={150} height={40} data={data2}>
+          <Bar dataKey="uv" fill="#8884d8" />
+        </BarChart>
       </div>
     </div>
   );
